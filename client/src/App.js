999d9b340a254b4acb045395a6552c125ac0f1a1
zprpic <prpiczvonimirprpiczvonimir@gmail.com>
2021-10-08 10:46:20 +0200

    Routing done. Styling navbar...

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,22 @@
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
+
+import Cart from "./components/Cart";
+import Home from "./components/Home";
+import NavBar from "./components/NavBar";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <NavBar />
+        <Switch>
+          <Route path="/cart" component={Cart} />
+          <Route path="/" component={Home} />
+        </Switch>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
