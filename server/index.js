0ead6b46986564ca12caa15cf3d8ea1020dcc79a
zprpic <prpiczvonimirprpiczvonimir@gmail.com>
2021-10-08 11:41:30 +0200

    Fake backend with API complete... redux time...

--- server/index.js
+++ server/index.js
@@ -3,11 +3,17 @@ const cors = require("cors");
 
 const app = express();
 
+const products = require("./products");
+
 app.use(express.json());
 app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("welcome to our online shop API");
+});
+
+app.get("/products", (req, res) => {
+  res.send(products);
 });
 
 const port = process.env.port || 5000;
---
+++ server/products.js
@@ -0,0 +1,28 @@
+const products = [
+  {
+    id: 1,
+    name: "iPhone 12 Pro",
+    description: "6.1-inch display",
+    price: 999,
+    image:
+      "https://res.cloudinary.com/zprpic/image/upload/v1633685831/iphone12_x4je9y.jpg",
+  },
+  {
+    id: 2,
+    name: "iPhone 12",
+    description: "5.4-inch display",
+    price: 699,
+    image:
+      "https://res.cloudinary.com/zprpic/image/upload/v1633685831/iphone12_x4je9y.jpg",
+  },
+  {
+    id: 3,
+    name: "Galaxy S",
+    description: "6.5-inch display",
+    price: 399,
+    image:
+      "https://res.cloudinary.com/zprpic/image/upload/v1633685831/galaxyS_gvofsn.png",
+  },
+];
+
+module.exports = products;
