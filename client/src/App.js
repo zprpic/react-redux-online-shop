41050371c3a2ef26afd8c34533815ca435e0b639
zprpic <prpiczvonimirprpiczvonimir@gmail.com>
2021-10-09 12:13:27 +0200

    React toastify added...

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,18 @@
 import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
 
 import Cart from "./components/Cart";
 import Home from "./components/Home";
 import NotFound from "./components/NotFound";
 import NavBar from "./components/NavBar";
 import "./App.css";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <ToastContainer />
         <NavBar />
         <Switch>
           <Route path="/cart" exact component={Cart} />
--- client/src/features/cartSlice.js
+++ client/src/features/cartSlice.js
@@ -1,4 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { toast } from "react-toastify";
 
 const initialState = {
   cartItems: [],
@@ -17,10 +18,21 @@ const cartSlice = createSlice({
 
       if (itemIndex >= 0) {
         state.cartItems[itemIndex].cartQuantity += 1;
+        toast.info(
+          `increased ${state.cartItems[itemIndex].name} cart quantity`,
+          {
+            position: "bottom-left",
+          }
+        );
       } else {
         const tempProduct = { ...action.payload, cartQuantity: 1 };
         state.cartItems.push(tempProduct);
+        toast.success(`${action.payload.name} added to cart`, {
+          position: "bottom-left",
+        });
       }
+
+      localStorage.setItem("cartItems", JSON.stringify(state.cartItems));
     },
   },
 });
