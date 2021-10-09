9a8b905beeae0b087f8f8de8435bab6accd2aacd
zprpic <prpiczvonimirprpiczvonimir@gmail.com>
2021-10-09 09:31:57 +0200

    Displaying data from RTK query

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -2,7 +2,34 @@ import { useGetAllProductsQuery } from "../features/productsAPI";
 
 const Home = () => {
   const { data, error, isLoading } = useGetAllProductsQuery();
-  return <h2>Home</h2>;
+  return (
+    <div className="home-container">
+      {isLoading ? (
+        <p>Loading</p>
+      ) : error ? (
+        <p>An error has occured...</p>
+      ) : (
+        <>
+          <h2>New Arrivals</h2>
+          <div className="products">
+            {data?.map((product) => {
+              return (
+                <div key={product.id} className="product">
+                  <h3>{product.name}</h3>
+                  <img src={product.image} alt={product.name} />
+                  <div className="details">
+                    <span>{product.description}</span>
+                    <span className="price">${product.price}</span>
+                  </div>
+                  <button>Add to cart</button>
+                </div>
+              );
+            })}
+          </div>
+        </>
+      )}
+    </div>
+  );
 };
 
 export default Home;
